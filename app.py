07b14a20c7155900c0dc5eb2ac874768bd61f427
Alex <alexhor22@gmail.com>
2020-07-23 20:08:43 -0500

    Requirements for port binding

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ def get_restaurants():
 if __name__ == '__main__':
     from api import *
     create_db()
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000)) 
+	app.run(host='0.0.0.0', port=port)
